8ba763124f647cb92eaf38fedb34bec96e84a6ad
Het Patel <hetpatel3245@gmail.com>
2023-07-16 23:24:24 +0530

    Image-slider added
    Image-slider has been added in the hero section and some bugs in the mobile view related to responsive-ui have been fixed. (navbar, last footer...).

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
 var alerted = localStorage.getItem('alerted') || '';
-    if (alerted != 'yes') {
-     alert("Welcome to the clone page of 'amazon.com'.\n(This is not the official site of Amazon.)");
-     localStorage.setItem('alerted','yes');
+if (alerted != 'yes') {
+    alert("Welcome to the clone page of 'amazon.com'.\n(This is not the official site of Amazon.)");
+    localStorage.setItem('alerted', 'yes');
+}
+
+
+// Image slider
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
